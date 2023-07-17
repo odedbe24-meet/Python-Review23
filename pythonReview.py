43d36d4f88cc05ac6d9908ab47ef65c87d6cbe71
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-17 12:08:26 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,21 @@
 def create_youtube_video(title, description):
 	video = {"Title": title, "Description" : description, "Likes": 0, "Dislikes": 0, "Comments": {}, "Hashtag":[]}
+	print("Like?")
+	if(input() == 'y'):
+		like(youtube_video)
+
+	print("Dislike?")
+	if(input() == 'y'):
+		dislike(youtube_video)
+
+	print("Would You Like To Add A Comment?")
+	if(input() == 'y'):
+		add_comment(youtube_video, input("What Is Your username?: "), input("what is the body of the comment"))
+	# print(youtube_video)
+
+	add_hashtag(youtube_video)
+	print("test test")
+
 	return video
 
 
@@ -23,26 +39,15 @@ def add_hashtag(video):
 			video["Hashtag"].append(hashtag)
 	
 
-def simil
+# def similarity_to_video(video1, video2):
+# 	if(len(video1["Hashtag"]) < len(video2["hashtag"]) )
+# 		print("true")
+
 
 
 youtube_video = create_youtube_video(input("title: "), input("Description: "))
-print(youtube_video)
 
-print("Like?")
-if(input() == 'y'):
-	like(youtube_video)
 
-print("Dislike?")
-if(input() == 'y'):
-	dislike(youtube_video)
-
-print("Would You Like To Add A Comment?")
-if(input() == 'y'):
-	add_comment(youtube_video, input("What Is Your username?: "), input("what is the body of the comment"))
-print(youtube_video)
-
-add_hashtag(youtube_video)
 # print(youtube_video)
 
 print("The Title is: " + youtube_video["Title"])
